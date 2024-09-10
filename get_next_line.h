ac12975d6a3d687d3616fa465ca346468152d497
Kay Freyer <kay.freyer@icloud.com>
2024-09-10 10:52:31 +0200

    norm

--- get_next_line.h
+++ get_next_line.h
@@ -13,24 +13,15 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 32
-#endif
-
-#include <unistd.h>
-#include <stdlib.h>
-
-# ifdef __cplusplus
-extern "C"
-{
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
 # endif
 
-int get_next_line(int fd, char **line);
+# include <stdlib.h>
+# include <unistd.h>
+
+int		get_next_line(int fd, char **line);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s);
-
-# ifdef __cplusplus
-}
-# endif
 
 #endif
