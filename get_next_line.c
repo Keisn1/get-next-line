e4f10eec75ec30bf181a3d1b64c7bf75f5f4f318
Kay Freyer <kay.freyer@icloud.com>
2024-09-12 17:44:02 +0200

    eliminate DELETE_STASH

--- get_next_line.c
+++ get_next_line.c
@@ -29,26 +29,16 @@ char	*crud_stash(t_op op, char *new_stash, int fd)
 	static char	*stash[MAX_FD];
 	char		*tmp;
 
-	if (op == SET_STASH)
-	{
-		free(stash[fd]);
-		stash[fd] = ft_strdup(new_stash);
-	}
-	if (op == DELETE_STASH)
-	{
-		free(stash[fd]);
-		stash[fd] = NULL;
-	}
-	if (op == UPDATE_STASH)
-	{
-		tmp = ft_strjoin(stash[fd], new_stash);
-		free(stash[fd]);
-		stash[fd] = tmp;
-	}
 	if (op == GET_STASH)
-		if (stash[fd])
-			return (ft_strdup(stash[fd]));
+		return (ft_strdup(stash[fd]));
+	if (op == SET_STASH)
+		tmp = ft_strdup(new_stash);
+	if (op == UPDATE_STASH)
+		tmp = ft_strjoin(stash[fd], new_stash);
+	free(stash[fd]);
+	stash[fd] = tmp;
 	return (NULL);
+
 }
 
 char	*truncate_stash(int fd)
@@ -67,14 +57,14 @@ char	*truncate_stash(int fd)
 		{
 			ret = ft_substr(cur_stash, 0, count);
 			if (count == ft_strlen(cur_stash))
-				crud_stash(DELETE_STASH, NULL, fd);
+				crud_stash(SET_STASH, NULL, fd);
 			else
 				crud_stash(SET_STASH, cur_stash + count, fd);
 			free(cur_stash);
 			return (ret);
 		}
 	}
-	crud_stash(DELETE_STASH, NULL, fd);
+	crud_stash(SET_STASH, NULL, fd);
 	return (cur_stash);
 }
 
@@ -98,6 +88,6 @@ char	*get_next_line(int fd)
 	free(buffer);
 	buffer = NULL;
 	if (bytes_read < 0)
-		return (crud_stash(DELETE_STASH, NULL, fd));
+		return (crud_stash(SET_STASH, NULL, fd));
 	return (truncate_stash(fd));
 }
