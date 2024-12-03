6dadc895c2c0d8933445428368d7ca01334518c1
Kay Freyer <kfreyer@c3r3s1.42wolfsburg.de>
2024-12-03 12:06:50 +0100

    no need for function get_empty_string

--- get_next_line.h
+++ get_next_line.h
@@ -35,7 +35,6 @@ typedef enum _OP
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s);
-char	*ft_get_empty_str(size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
